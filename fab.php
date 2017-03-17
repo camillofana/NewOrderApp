0f4c914ec48832e56ecfa9e986f93c9797721623
monzanifabio <renderbros@gmail.com>
2017-03-18 00:32:30 +1100

    FAB Floating Action Button added and minor CSS updates
    Floating action button added.
    Orders and fab are included from .php files.
    fab.css added
    fab.php added
    materialize.js added in order to make the fab works.

---
+++ fab.php
@@ -0,0 +1,11 @@
+      <div class="fixed-action-btn click-to-toggle">
+          <a class="btn-floating btn-large orderlyblue">
+            <i class="large material-icons">mode_edit</i>
+          </a>
+          <ul>
+            <li><a class="btn-floating orderlyblue"><span class="glyphicon glyphicon-th-large" aria-hidden="true"></span></a></li>
+            <li><a class="btn-floating orderlyblue"><span class="glyphicon glyphicon-hand-right" aria-hidden="true"></span></a></li>
+            <li><a class="btn-floating orderlyblue"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></a></li>
+            <li><a class="btn-floating orderlyblue"><span class="glyphicon glyphicon-user" aria-hidden="true"></a></li>
+          </ul>
+        </div>
--- index.php
+++ index.php
@@ -13,6 +13,10 @@
     <!-- Include Bootstrap CSS -->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import Floating action button CSS -->
+    <link type="text/css" rel="stylesheet" href="css/fab.css"  media="screen,projection"/>
     <!-- Include main CSS -->
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <!-- Include jQuery library -->
@@ -77,81 +81,15 @@
                 <button href="#menu-toggle" id="menu-toggle" type="button" class="btn btn-default navbar-btn pull-left">
                   <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
                 </button>
-                <h3 class="navbar-text">AngularJS CRUD Operations Demo</h3>
+                <h3 class="navbar-text">AngularJS CRUD Operations Demo</h3> <!-- Navbar title - replace with current section -->
               </div>
             </div>
           </nav>
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                      <div ng-controller="DbController">
-                          <nav class="navbar navbar-default">
-                            <div class="navbar-header">
-                              <div class="alert alert-default navbar-brand search-box">
-                                <button class="btn btn-primary" ng-show="show_form" ng-click="formToggle()">Add Order <span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
-                              </div>
-                              <div class="alert alert-default input-group search-box">
-                                <span class="input-group-btn">
-                                  <input type="text" class="form-control" placeholder="Live search for product name, provide etc..." ng-model="search_query">
-                                </span>
-                              </div>
-                            </div>
-                          </nav>
-                          <div class="col-md-6 col-md-offset-3">
-
-                      <!-- Include form template which is used to insert data into database -->
-                        <div ng-include src="'templates/form.html'"></div>
-
-                      <!-- Include form template which is used to edit and update data into database -->
-                        <div ng-include src="'templates/editForm.html'"></div>
-                      </div>
-                      <div class="clearfix"></div>
-
-                      <!-- Table to show order detalis -->
-                      <div id="table_content"></div>
-
-                      <!-- Table to show employee detalis -->
-                      <div class="table-responsive">
-                        <table class="table table-hover">
-                          <tr>
-                            <th>ORDER ID</th>
-                            <th>PROVIDER</th>
-                            <th>SENDER</th>
-                            <th>CREATOR</th>
-                            <th></th>
-                          </tr>
-                          <tr ng-repeat="detail in details| filter:search_query">
-                            <td>{{detail.OrderNumber}}</td>
-                            <td>{{detail.name}}</td>
-                            <td>{{detail.sender}}</td>
-                            <td>{{detail.user_name}}</td>
-                            <td>
-                              <button class="btn btn-info" ng-click="orderDetail(detail)" title="Detail"><span class="glyphicon glyphicon-eye-open"></span></button>
-                              <button class="btn btn-warning" ng-click="editInfo(detail)" title="Edit"><span class="glyphicon glyphicon-edit"></span></button>
-                              <button class="btn btn-danger" ng-click="deleteInfo(detail)" title="Delete"><span class="glyphicon glyphicon-trash"></span></button>
-                            </td>
-                          </tr>
-                        </table>
-                      </div>
-
-                      	<!-- Table to show employee detalis -->
-                      <div class="table-responsive">
-                          <table class="table table-hover">
-                              <tr>
-                                  <th>ID</th>
-                                  <th>Product</th>
-                                  <th>Amount</th>
-                                  <th>Unit</th>
-                              </tr>
-                              <tr ng-repeat="Odetail in Odetails| filter:search_query">
-                                  <td>{{Odetail.id}}</td>
-                                  <td>{{Odetail.name}}</td>
-                                  <td>{{Odetail.ammount}}</td>
-                                  <td>{{Odetail.unit}}</td>
-                              </tr>
-                          </table>
-                      </div>
-                    </div>
+                      <?php include 'orders.php';?> <!-- Recall orders.php page -->
+                      <?php include 'fab.php';?> <!-- Recall floating button -->
                   </div>
                 </div>
               </div>
@@ -164,6 +102,9 @@
 
     <!-- Include controller -->
     <script src="js/angular-script.js"></script>
+    <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
     	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
